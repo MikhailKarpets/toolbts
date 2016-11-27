fdf7f81f381356fb3cc3c721db0d244a7a3a3c64
MikhailKarpets <ruberoid99@ya.ru>
2016-11-27 16:42:47 +0300

    adding text file

--- karpets.py
+++ karpets.py
@@ -10,6 +10,16 @@ import numpy as np
 
 data = pd.read_csv('F:\mike\hse\exactpro\JBoss.csv')
 
-data_to_print = data['Description'][:5];
+#data_to_print = data['Description'][:5];
+#print(data_to_print)
 
-print(data_to_print)
+data_to_write = data['Description'][:3]
+
+f = open("F:\mygit\descr.txt","r")
+o = open("F:\\mygit\\text.txt",'w', newline="\r\n")
+for line in f:
+    print(line)
+    o.write(line)
+o.close()
+f.close()
+
