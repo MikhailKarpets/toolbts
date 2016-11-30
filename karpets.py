b7db7bb6112d90c576e9dd0b71f91c8688c75a37
MikhailKarpets <ruberoid99@ya.ru>
2016-11-30 04:09:38 +0300

    stack trace 3

--- karpets.py
+++ karpets.py
@@ -7,89 +7,49 @@ Created on Sun Nov 27 14:31:42 2016
 
 import pandas as pd
 import re
-import numpy as np
-
-
-data = pd.read_csv('F:\mike\hse\exactpro\JBoss.csv')
-data_description0 = data['Description'][1:]
-data_description0.dropna(inplace = True)
-data_description0.to_frame()
-data_len = len(data_description0)
-zeros_df = pd.DataFrame(0, index=data_description0.index, columns=['HasStackTrace'])
-data_description = pd.concat([data_description0, zeros_df], axis=1)
-
-#data_description = data_description.as_matrix()
-subset = data_description[['Description', 'HasStackTrace']]
-list_data_descr_and_stack_trace_flag = [list(x) for x in subset.values]
-
-
-
-#pattern = re.compile('at .*java.*\\n+')
-#pattern = re.compile('\\n.*ERROR.*')
-#pattern = re.compile('(.*ERROR.*\\n)+.*[a-zA-Z]Exception.*')
-#pattern = re.compile('(.*ERROR.*\\n)+?.*[a-zA-Z]Exception.*')
-#ok below (106)
-#pattern = re.compile('(.*ERROR.*\\n)+?(.*[a-zA-Z]Exception.*)+?')
-#pattern = re.compile('(^ERROR.*\\n)?(.*ERROR.*\\n)+?(.*[a-zA-Z]Exception.*\\n)+?')
-#OK below (103&106)
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?')
-#OK below (103&106)
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*java.*\\n)+)')
-#OK below
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*java.*[)].*\\n)+)')
-#OK
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*java.*[)].*)+)')
-#OK
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+)')
-#OK except 249
-#pattern = re.compile('((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+)')
-
-#LAST VARIANT (29/11/2016 15.00)
-pattern = re.compile('(?:(((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+))|((((?:((\\n.*ERROR.*)+)|((.*ERROR.*\\n)+)))+)((((.*[a-zA-Z]Exception.*\\n)+)))))')
-
-
-#(((.*[a-zA-Z]Exception.*\\n)+)?)
-#pattern = re.compile('((\\n.*ERROR.*)+?(\\n.*[a-zA-Z0-9]Exception.*)?)?\\n.*at .*([^\\n\s]*[.][^\\n\s]*)+(java)?.*[(].*[)]')
-#ok below (except 3,106 etc)
-#pattern = re.compile('((\\n.*ERROR.*)+?(\\n.*[a-zA-Z0-9]Exception.*)?)?(\\n.*at .*([^\\n\s]*[.][^\\n\s]*)+(java)?.*[(].*[)](\\nCaused by:.*)?(\\n([^\\n\s]*[.][^\\n\s]*)+.*[(].*java.*[)])?)+')
-#pattern = re.compile('((((^|\\n).*ERROR.*)+)?(\\n.*[a-zA-Z0-9]Exception.*)?)?(\\n.*at .*([^\\n\s]*[.][^\\n\s]*)+(java)?.*[(].*[)](\\nCaused by:.*)?(\\n([^\\n\s]*[.][^\\n\s]*)+.*[(].*java.*[)])?)+')
-#pattern = re.compile('((((^|\\n).*ERROR.*)+)?(\\n.*[a-zA-Z]Exception.*)?)?(\\n.*at .*([^\\n\s]*[.][^\\n\s]*)+.*[(].*java.*[)](\\nCaused by:.*)?(\\n([^\\n\s]*[.][^\\n\s]*)+.*[(].*java.*[)])?)+')
-
-for i,item in enumerate(list_data_descr_and_stack_trace_flag):
-    text = item[0]
-    print(text)
-    if pattern.search(text):
-        list_data_descr_and_stack_trace_flag[i][1] = 1
-        temp_str = pattern.sub('', text)
-        temp_str = " ".join(temp_str.split())
-        list_data_descr_and_stack_trace_flag[i][0] = temp_str
-print(list_data_descr_and_stack_trace_flag)
+#import numpy as np
+number_of_bug_descr = list()
+all_projects_list = list()
+for i in range(2,3):
+    str_path = "F:\\mike\\hse\\exactpro\\all_projects\\JBoss%d.csv" % i
+    data = pd.read_csv(str_path)
+    data_description0 = data['Description'][1:]
+    data_description0.dropna(inplace = True)
+    data_description0.to_frame()
+    data_len = len(data_description0)
+    number_of_bug_descr.append(data_len)
+    zeros_df = pd.DataFrame(0, index=data_description0.index, columns=['HasStackTrace'])
+    data_description = pd.concat([data_description0, zeros_df], axis=1)
     
-
-
-
-#for index,row in data_description.iterrows():
-##    print(row['Description'])
-#    text = data_description.iloc[index]['Description']
-##    print(text)
-#    if isinstance(text, str):
-#        if pattern.search(text):
-##            print('----------------------------BEGIN-------------------------')
-##            print('-------------------------'+str(index)+'----------------------------')
-##            print(row)
-##            print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-#            temp_str = pattern.sub('', text)
-#            temp_str = " ".join(temp_str.split())
-#            data_description['Description'][index] = temp_str
-##            print(data_description[index])
-##            matches = pattern.findall(row)
-##            print(matches)
-##            print('-------------------------'+str(index)+'----------------------------')
-##            print('-------------------------END----------------------------')
-##            print()
-##            print()
-#    else:
-#        print(type(row))
-#        print(row)
-#for x in data_description:
-#    print(x)
+    subset = data_description[['Description', 'HasStackTrace']]
+    list_data_descr_and_stack_trace_flag = [list(x) for x in subset.values]
+    
+    #LAST VARIANT (29/11/2016 15.00)
+    pattern = re.compile('(?:(((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+))|((((?:((\\n.*ERROR.*)+)|((.*ERROR.*\\n)+)))+)((((.*[a-zA-Z]Exception.*\\n)+)))))')
+    
+    if i == 2:
+        del list_data_descr_and_stack_trace_flag[58]
+        del list_data_descr_and_stack_trace_flag[810]
+    
+    for k,item in enumerate(list_data_descr_and_stack_trace_flag):
+#        print(k)
+        text = item[0]
+    #    print(text)
+        if pattern.search(text):
+            list_data_descr_and_stack_trace_flag[k][1] = 1
+            temp_str = pattern.sub('', text)
+            temp_str = " ".join(temp_str.split())
+            list_data_descr_and_stack_trace_flag[k][0] = temp_str
+    all_projects_list = all_projects_list + list_data_descr_and_stack_trace_flag
+#    print(list_data_descr_and_stack_trace_flag)
+#print(all_projects_list)
+print('Number of all descriptions: ')
+print(len(all_projects_list))
+print('Number of descriptions from projects 1,2,3: ')
+print(number_of_bug_descr)
+for j in range(80,100):
+    print('|||||||||||||||||||||BEGIN||||||||||||||||||||')
+    print(j)
+    print('|||||||||||||||||||||BEGIN||||||||||||||||||||||||||||||')
+    print(all_projects_list[j][0])
+    
