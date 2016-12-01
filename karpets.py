e1bdcbfab813d8598204684edbcc2994c38f14da
MikhailKarpets <ruberoid99@ya.ru>
2016-12-01 19:08:30 +0300

    stack trace 5

--- karpets.py
+++ karpets.py
@@ -10,32 +10,47 @@ import re
 #import numpy as np
 
 
-def check_pattern(pattern0, text, num_pattern):
-    if pattern0.search(text):
-        temp_str = 'PATTERN %d' % num_pattern  
-        print(temp_str) 
-    #   list_data_descr_and_stack_trace_flag[k][1] = 1
-        temp_str = pattern0.sub('', text)
-    #   temp_str = " ".join(temp_str.split())
-        print('|||||||||||||||||||||||||||||||BEGIN MATCHES|||||||||||||||||||||||||||')
-        matches = pattern0.findall(text)
-        print(matches)
-        print('|||||||||||||||||||||||||||||||END MATCHES|||||||||||||||||||||||||||||')
-        print()
-        print()
-        print('|||||||||||||||||||||||||||||||BEGIN clean TEXT|||||||||||||||||||||||||||')
-        print(temp_str)
-        print('|||||||||||||||||||||||||||||||END clean TEXT|||||||||||||||||||||||||||')
-        print()
-        print()    
-        return temp_str
-    else:
-        return text
+def filter_pattern(arr_pattern, text):
+    stack_trace_flag = 0
+    temp_str = ' '
+    cleaned_text = ' '
+    cleaned_text = text
+    temp_str = text
+    for i, pattern in enumerate(arr_pattern):
+        print('.')
+        if pattern.search(temp_str):
+            pattern_declaration = 'PATTERN %d' % i  
+            print(pattern_declaration)
+            if ((i < 12) and (stack_trace_flag == 0)): #change i if number of stack-trace regex changes! 
+                stack_trace_flag = 1
+#            matches = pattern.findall(temp_str)
+            temp_str = pattern.sub('', temp_str)
+            cleaned_text = temp_str
+#            temp_str = " ".join(temp_str.split())
+#            print('|||||||||||||||||||||||||||||||BEGIN MATCHES||||||||||||||||||||||||||||||')
+#            print(matches)
+#            print('|||||||||||||||||||||||||||||||END MATCHES||||||||||||||||||||||||||||||||')
+#            print()
+#            print()
+#            print('|||||||||||||||||||||||||||||||BEGIN cleaned TEXT|||||||||||||||||||||||||||')
+#            print(cleaned_text)
+#            print('|||||||||||||||||||||||||||||||END cleaned TEXT|||||||||||||||||||||||||||||')
+#            print()
+#            print()    
+    print()
+    print()
+    print('|||||||||||||||||||||||||||||||BEGIN cleaned TEXT|||||||||||||||||||||||||||')
+    cleaned_text = " ".join(cleaned_text.split())
+    print(cleaned_text)
+    print('|||||||||||||||||||||||||||||||END cleaned TEXT|||||||||||||||||||||||||||||')
+#    print()
+#    print()
+    return [temp_str, stack_trace_flag]
 
 
 number_of_bug_descr = list()
-all_projects_list = list()
-for i in range(2,3):
+list_of_all_projects_cleaned_bugs_descriptions_with_stack_trace_flags = list()
+for i in range(1,4):
     str_path = "F:\\mike\\hse\\exactpro\\all_projects\\JBoss%d.csv" % i
     data = pd.read_csv(str_path)
     data_description0 = data['Description'][1:]
@@ -47,75 +62,64 @@ for i in range(2,3):
     data_description = pd.concat([data_description0, zeros_df], axis=1)
     
     subset = data_description[['Description', 'HasStackTrace']]
-    list_data_descr_and_stack_trace_flag = [list(x) for x in subset.values]
+    list_of_bugs_descriptions_and_stack_trace_flags = [list(x) for x in subset.values]
     
     #LAST VARIANT (01/12/2016 00.00)
-    pattern0 = re.compile('(?:(((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+))|((((?:((\\n.*ERROR.*)+)|((.*ERROR.*\\n)+)))+)((((.*[a-zA-Z]Exception.*\\n)+)))))')
-    pattern1 = re.compile('(  ?:.*ERROR.*|(?:.*INFO.*|(?:.*WARN.*|(?:.*CLOSE_WAIT.*|(?:.*BLOCKED.*|(?:.*DEBUG.*|.*WAITING.*))))))') 
-#    сомнения в работоспособности (ниже)
-#    pattern2 = re.compile('(?:((.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*[[]error[]].*client.*)+)|(?:((.*at line.*\\n.*at line.*\\n.*at line.*\\n)+)|(?:((.*java.*[(].*[)].*\\n.*java.*[(].*[)].*\\n.*java.*[(].*[)].*\\n.*java.*[(].*[)].*\\n)+)|(?:((.*[Cc]aused by.*java.*\\n.*[Cc]aused by.*java.*\\n)+)|( ( [a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+[(](?:.*java.*|(?:.*Native Method.*|.*No such file or directory.*))[)])+)))))')
-    
+    pattern0 = re.compile('(?:.*ERROR.*|(?:.*INFO.*|(?:.*WARN.*|(?:.*CLOSE_WAIT.*|(?:.*BLOCKED.*|(?:.*DEBUG.*|.*WAITING.*))))))') 
+    pattern1 = re.compile('(?:(((\\n.*ERROR.*)+)?((.*ERROR.*\\n)+)?((.*[a-zA-Z]Exception.*\\n)+)?((.*[a-zA-Z]Error.*\\n)+)?((.*at .*[(].*(?:java|(?:Unknown Source|Native Method)).*[)].*)+))|((((?:((\\n.*ERROR.*)+)|((.*ERROR.*\\n)+)))+)((((.*[a-zA-Z]Exception.*\\n)+)))))')
+    pattern2 = re.compile('.*(?:waiting|locked).*0x.*[(].*[)].*')
+    pattern3 = re.compile('.*0x.*0x.*')
+    pattern4 = re.compile('.*(?:(".*ActiveMQ.*")|(".*Thread-7.*")).*')
 #    ok
-    pattern2 = re.compile('([a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+[(](?:.*java.*|(?:.*Native Method.*|.*No such file or directory.*))[)])')
+    pattern5 = re.compile('([a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+[(](?:.*java.*|(?:.*Native Method.*|.*No such file or directory.*))[)])')
 #    ok
-    pattern3 = re.compile('.*[Cc]aused by.*java.*\\n(.*[Cc]aused by.*java.*\\n)+')
+    pattern6 = re.compile('.*[Cc]aused by.*java.*\\n(.*[Cc]aused by.*java.*\\n)+')
 #    ok    
-    pattern4 = re.compile('.*at line.*\\n.*at line.*\\n((.*at line.*\\n)+)')
+    pattern7 = re.compile('.*at line.*\\n.*at line.*\\n((.*at line.*\\n)+)')
 #    ok
-    pattern5 = re.compile('((.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*[[]error[]].*client.*)+)')
+    pattern8 = re.compile('((.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*[[]error[]].*client.*)+)')
 #    ok
-    pattern6 = re.compile('.*java.*[(].*[)].*\\n(.*java.*[(].*[)].*\\n)+')
-    pattern7 = re.compile('.*ERROR.*\\n')
-    pattern8 = re.compile('.*[(].*".*".*=>.*".*".*[)].*')
-    pattern9 = re.compile('failure description:.*[{].*[}]', re.DOTALL)
-    pattern10 = re.compile('.*WFLYPRT.*')
+    pattern9 = re.compile('.*java.*[(].*[)].*\\n.*java.*[(].*[)].*\\n')
+#    pattern7 = re.compile('.*ERROR.*\\n')
+    pattern10 = re.compile('.*[(].*".*".*=>.*".*".*[)].*')
+    pattern11 = re.compile('failure description:.*[{].*[}]', re.DOTALL)
+#    pattern11 = re.compile('.*WFLYPRT.*')
     
-    arr_patterns = [pattern0, pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9]
-   
-    if i == 2:
-        del list_data_descr_and_stack_trace_flag[58]
-        del list_data_descr_and_stack_trace_flag[810]
+    arr_patterns = [pattern0, pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9, pattern10, pattern11]
+
     
-    for k,item in enumerate(list_data_descr_and_stack_trace_flag):
-        if k < 16:
-            continue
-        if k == 25:
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            print('|||||||||||||||||||||||||||||||||||||||||||')
-            break
+    for k,item in enumerate(list_of_bugs_descriptions_and_stack_trace_flags):
+#        if k < 597:
+#            continue
+#        if k == 598:
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            print('|||||||||||||||||||||||||||||||||||||||||||')
+#            break
         print('|||||||||||NUMBER OF TEXT||||||||||||')
         print(k)
         text = item[0]
-        print('|||||||||||||||||||||||||||||||BEGIN TEXT with trash|||||||||||||||||||||||||||')
-        print(text)
-        print('|||||||||||||||||||||||||||||||END TEXT with trash|||||||||||||||||||||||||||')
+#        print('|||||||||||||||||||||||||||||||BEGIN TEXT with trash|||||||||||||||||||||||||||')
+#        print(text)
+#        print('|||||||||||||||||||||||||||||||END TEXT with trash|||||||||||||||||||||||||||')
         print()
         print()
-        for i, pattern in enumerate(arr_patterns):
-            text = check_pattern(pattern, text, i)
-            
-                
+        list_of_bugs_descriptions_and_stack_trace_flags[k] = filter_pattern(arr_patterns, text)  
+    list_of_all_projects_cleaned_bugs_descriptions_with_stack_trace_flags = list_of_all_projects_cleaned_bugs_descriptions_with_stack_trace_flags + list_of_bugs_descriptions_and_stack_trace_flags
+#        print('-------------------THIS IS THE END-----------------------')
+    str_for_print = 'Number of descriptions in %d project: ' % i
+    print(str_for_print)
+    print(len(list_of_bugs_descriptions_and_stack_trace_flags))
 
-            
-#            list_data_descr_and_stack_trace_flag[k][0] = temp_str
-#    all_projects_list = all_projects_list + list_data_descr_and_stack_trace_flag
-#    print(list_data_descr_and_stack_trace_flag)
-#print(all_projects_list)
-#print('Number of all descriptions: ')
-#print(len(all_projects_list))
-#print('Number of descriptions from projects 1,2,3: ')
-#print(number_of_bug_descr)
-#for j in range(80,100):
-#    print('|||||||||||||||||||||BEGIN||||||||||||||||||||')
-#    print(j)
-#    print('|||||||||||||||||||||BEGIN||||||||||||||||||||||||||||||')
-#    print(all_projects_list[j][0])
-    
+
+print(list_of_all_projects_cleaned_bugs_descriptions_with_stack_trace_flags)
+print('Number of all descriptions: ')
+print(len(list_of_all_projects_cleaned_bugs_descriptions_with_stack_trace_flags))
+ 
