97efd129b2a12fdaa4e3b93fe6ac1fa32e807d84
MikhailKarpets <ruberoid99@ya.ru>
2016-12-02 03:05:05 +0300

    stack trace 9

--- karpets.py
+++ karpets.py
@@ -11,9 +11,9 @@ import re
 
 
 def filter_pattern(arr_pattern, text):
-    print('||||||||||||||||||||RAW TEXT|||||||||||||||||||||||')
-    print(text)
-    print('||||||||||||||||||||RAW TEXT|||||||||||||||||||||||')
+#    print('||||||||||||||||||||RAW TEXT|||||||||||||||||||||||')
+#    print(text)
+#    print('||||||||||||||||||||RAW TEXT|||||||||||||||||||||||')
     print()
     stack_trace_flag = 0
     temp_str = ' '
@@ -24,18 +24,18 @@ def filter_pattern(arr_pattern, text):
 #        print('.')
         if pattern.search(temp_str):
             pattern_declaration = 'PATTERN %d' % i  
-            print(pattern_declaration)
+#            print(pattern_declaration)
             if ((i < 12) and (stack_trace_flag == 0)): #change i if number of stack-trace regex changes! 
                 stack_trace_flag = 1
             matches = pattern.findall(temp_str)
             temp_str = pattern.sub('', temp_str)
             cleaned_text = temp_str
 #            temp_str = " ".join(temp_str.split())
-            print('|||||||||||||||||||||||||||||||BEGIN MATCHES||||||||||||||||||||||||||||||') #mt
-            print(matches) #mt
-            print('|||||||||||||||||||||||||||||||END MATCHES||||||||||||||||||||||||||||||||') #mt
-            print() #mt
-            print() #mt 
+#            print('|||||||||||||||||||||||||||||||BEGIN MATCHES||||||||||||||||||||||||||||||') #mt
+#            print(matches) #mt
+#            print('|||||||||||||||||||||||||||||||END MATCHES||||||||||||||||||||||||||||||||') #mt
+#            print() #mt
+#            print() #mt 
 #            print('|||||||||||||||||||||||||||||||BEGIN cleaned TEXT|||||||||||||||||||||||||||')
 #            print(cleaned_text)
 #            print('|||||||||||||||||||||||||||||||END cleaned TEXT|||||||||||||||||||||||||||||')
@@ -63,28 +63,31 @@ pattern8 = re.compile('((.*[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*[[]error[]].*client
 pattern9 = re.compile('.*java.*[(].*[)].*\\n.*java.*[(].*[)].*\\n')
 #    pattern7 = re.compile('.*ERROR.*\\n')
 pattern10 = re.compile('.*[(].*".*".*=>.*".*".*[)].*')
-#pattern11 = re.compile('failure description:.*[{].*[}]', re.DOTALL)
+pattern11 = re.compile('failure description:.*[{].*[}]', re.DOTALL)
 #    pattern11 = re.compile('.*WFLYPRT.*')
 #ok
 #pattern12 = re.compile('[\\s][{][\\s].*[\\s][}][\\s]', re.DOTALL)
-pattern11 = re.compile('\\n[{]\\r\\n.*\\r\\n[}]\\r', re.DOTALL)
-pattern12 = re.compile('[/][^\\s]*=[^\\s]*[/][^\\s]*=[^\\s]*')
-pattern13 = re.compile('[^\\s]+[.][^\\s"]+')
-pattern14 = re.compile('{{.*}}') 
-pattern15 = re.compile('[^\\s]+[/][^\\s]+')
-pattern16 = re.compile('.*Event.*receive.*from remote server.*\\nInternal Server Error.*')
-pattern17 = re.compile('<.*>\\s.*<[/].*>\\s', re.DOTALL)
-pattern18 = re.compile('\\s.*=.*\\s')
-pattern19 = re.compile('(?:({panel})|(?:({code[^\\s]*})|(?:({noformat})|({quote}))))')
-pattern20 = re.compile('[a-z]+[A-Z][a-z]+( )?[{].*[}]', re.DOTALL)
-pattern21 = re.compile('[A-Z][a-z]+[A-Z][a-z]+[(].*[)]')
-pattern22 = re.compile('{.*return.*}', re.DOTALL)
-pattern23 = re.compile('[^\\s]*@[^\\s]*')
-pattern24 = re.compile('[a-z]+[A-Z][a-z]+[^\\s]*')
+pattern12 = re.compile('\\n[{]\\r\\n.*\\r\\n[}]\\r', re.DOTALL)
+pattern13 = re.compile('[/][^\\s]*=[^\\s]*[/][^\\s]*=[^\\s]*')
+pattern14 = re.compile('[^\\s]+[.][^\\s"]+')
+pattern15 = re.compile('{{.*}}') 
+pattern16 = re.compile('[^\\s]+[/][^\\s]+')
+pattern17 = re.compile('.*Event.*receive.*from remote server.*\\nInternal Server Error.*')
+pattern18 = re.compile('<.*>\\s.*<[/].*>\\s', re.DOTALL)
+pattern19 = re.compile('\\s.*=.*\\s')
+pattern20 = re.compile('(?:({panel})|(?:({code[^\\s]*})|(?:({noformat})|({quote}))))')
+pattern21 = re.compile('[a-z]+[A-Z][a-z]+( )?[{].*[}]', re.DOTALL)
+pattern22 = re.compile('[A-Z][a-z]+[A-Z][a-z]+[(].*[)]')
+pattern23 = re.compile('{.*return.*}', re.DOTALL)
+pattern24 = re.compile('[^\\s]*@[^\\s]*')
+pattern25 = re.compile('[a-z]+[A-Z][a-z]+[^\\s]*')
+pattern26 = re.compile('[0-9]+.*has been deprecated')
+pattern27 = re.compile('[[][0-9][0-9]:[0-9][0-9] (?:(PM)|(AM))[]].*:')
+pattern28 = re.compile('@[^\\s]*')
 
 #Димино
-pattern25 = re.compile('(?:a2p|ac|addgroup|adduser|agrep|alias|apropos|apt-cache|apt-get|aptitude|ar|arch|arp|as|aspell|at|awk|basename|bash|bc|bdiff|bfs|bg|biff|break|bs|bye|cal|calendar|cancel|cat|cc|cd|cfdisk|chdir|checkeq|checknr|chfn|chgrp|chkey|chmod|chown|chroot|chsh|cksum|clear|cmp|col|comm|compress|continue|cp|cpio|crontab|csh|csplit|ctags|cu|curl|cut|date|dc|dd|delgroup|deluser|depmod|deroff|df|dhclient|diff|dig|dircmp|dirname|dmesg|dos2unix|dpkg|dpost|du|echo|ed|edit|egrep|eject|elm|emacs|enable|env|eqn|ex|exit|expand|expr|fc|fdisk|fg|fgrep|file|find|findsmb|finger|fmt|fold|for|foreach|free|fsck|ftp|fuser|gawk|getfacl|gpasswd|gprof|grep|groupadd|groupdel|groupmod|gunzip|gview|gvim|gzip|halt|hash|hashstat|head|help|history|host|hostid|hostname|id|ifconfig|ifdown|ifquery|ifup|info|init|insmod|iostat|ip|isalist|iwconfig|jobs|join|keylogin|kill|killall|ksh|last|ld|ldd|less|lex|link|ln|lo|locate|login|logname|logout|losetup|lp|lpadmin|lpc|lpq|lpr|lprm|lpstat|ls|lsmod|lsof|lzcat|lzma|mach|mail|mailcompat|mailx|make|man|merge|mesg|mii-tool|mkdir|mkfs|mkswap|modinfo|modprobe|more|mount|mt|mv|myisamchk|mysql|mysqldump|nc|neqn|netstat|newalias|newform|newgrp|nice|niscat|nischmod|nischown|nischttl|nisdefaults|nisgrep|nismatch|nispasswd|nistbladm|nl|nmap|nohup|nroff|nslookup|od|on|onintr|optisa|pack|pagesize|parted|partprobe|passwd|paste|pax|pcat|perl|pg|pgrep|pico|pine|ping|pkill|poweroff|pr|printenv|printf|priocntl|ps|pstree|pvs|pwd|quit|rcp|readlink|reboot|red|rehash|rename|renice|repeat|replace|rgview rgvim|rlogin|rm|rmdir|rmmod|rn|route|rpcinfo|rsh|rsync|rview|rvim|s2p|sag|sar|scp|screen|script|sdiff|sed|sendmail|service|set|setenv|setfacl|sfdisk|sftp|sh|shred|shutdown|sleep|slogin|smbclient|sort|spell|split|startx|stat|stop|strftime|strip|stty|su|sudo|swapoff|swapon|sysklogd|tabs|tac|tail|talk|tar|tbl|tcopy|tcpdump|tcsh|tee|telinit|telnet|test|time|timex|todos|top|touch|tput|tr|traceroute|trap|tree|troff|tty|ul|umask|umount|unalias|uname|uncompress|unhash|uniq|unlink|unlzma|unpack|until|unxz|unzip|uptime|useradd|userdel|usermod|vacation|vgrind|vi|view|vim|vipw|visudo|vmstat|w|wait|wall|wc|wget|whatis|whereis|which|while|who|whoami|whois|write|X|Xorg|xargs|xfd|xhost|xinit|xlsfonts|xrdb|xset|xterm|xz|xzcat|yacc|yes|yppasswd|yum|zcat|zip|zipcloak|zipinfo|zipnote|zipsplit) -{1,2}\w+ \w*')
-pattern26 = re.compile('(?:https|http)://\w*\S*\d*', re.DOTALL)
+pattern29 = re.compile('(?:a2p|ac|addgroup|adduser|agrep|alias|apropos|apt-cache|apt-get|aptitude|ar|arch|arp|as|aspell|at|awk|basename|bash|bc|bdiff|bfs|bg|biff|break|bs|bye|cal|calendar|cancel|cat|cc|cd|cfdisk|chdir|checkeq|checknr|chfn|chgrp|chkey|chmod|chown|chroot|chsh|cksum|clear|cmp|col|comm|compress|continue|cp|cpio|crontab|csh|csplit|ctags|cu|curl|cut|date|dc|dd|delgroup|deluser|depmod|deroff|df|dhclient|diff|dig|dircmp|dirname|dmesg|dos2unix|dpkg|dpost|du|echo|ed|edit|egrep|eject|elm|emacs|enable|env|eqn|ex|exit|expand|expr|fc|fdisk|fg|fgrep|file|find|findsmb|finger|fmt|fold|for|foreach|free|fsck|ftp|fuser|gawk|getfacl|gpasswd|gprof|grep|groupadd|groupdel|groupmod|gunzip|gview|gvim|gzip|halt|hash|hashstat|head|help|history|host|hostid|hostname|id|ifconfig|ifdown|ifquery|ifup|info|init|insmod|iostat|ip|isalist|iwconfig|jobs|join|keylogin|kill|killall|ksh|last|ld|ldd|less|lex|link|ln|lo|locate|login|logname|logout|losetup|lp|lpadmin|lpc|lpq|lpr|lprm|lpstat|ls|lsmod|lsof|lzcat|lzma|mach|mail|mailcompat|mailx|make|man|merge|mesg|mii-tool|mkdir|mkfs|mkswap|modinfo|modprobe|more|mount|mt|mv|myisamchk|mysql|mysqldump|nc|neqn|netstat|newalias|newform|newgrp|nice|niscat|nischmod|nischown|nischttl|nisdefaults|nisgrep|nismatch|nispasswd|nistbladm|nl|nmap|nohup|nroff|nslookup|od|on|onintr|optisa|pack|pagesize|parted|partprobe|passwd|paste|pax|pcat|perl|pg|pgrep|pico|pine|ping|pkill|poweroff|pr|printenv|printf|priocntl|ps|pstree|pvs|pwd|quit|rcp|readlink|reboot|red|rehash|rename|renice|repeat|replace|rgview rgvim|rlogin|rm|rmdir|rmmod|rn|route|rpcinfo|rsh|rsync|rview|rvim|s2p|sag|sar|scp|screen|script|sdiff|sed|sendmail|service|set|setenv|setfacl|sfdisk|sftp|sh|shred|shutdown|sleep|slogin|smbclient|sort|spell|split|startx|stat|stop|strftime|strip|stty|su|sudo|swapoff|swapon|sysklogd|tabs|tac|tail|talk|tar|tbl|tcopy|tcpdump|tcsh|tee|telinit|telnet|test|time|timex|todos|top|touch|tput|tr|traceroute|trap|tree|troff|tty|ul|umask|umount|unalias|uname|uncompress|unhash|uniq|unlink|unlzma|unpack|until|unxz|unzip|uptime|useradd|userdel|usermod|vacation|vgrind|vi|view|vim|vipw|visudo|vmstat|w|wait|wall|wc|wget|whatis|whereis|which|while|who|whoami|whois|write|X|Xorg|xargs|xfd|xhost|xinit|xlsfonts|xrdb|xset|xterm|xz|xzcat|yacc|yes|yppasswd|yum|zcat|zip|zipcloak|zipinfo|zipnote|zipsplit) -{1,2}\w+ \w*')
+pattern30 = re.compile('(?:https|http)://\w*\S*\d*', re.DOTALL)
 #pattern27 = re.compile(r'(?: |^)[a-zA-Z]+', re.DOTALL)
 #pattern20 = re.compile(r'.*?({{.*?}}).*?', re.DOTALL)
 #pattern21 = re.compile(r'.*?({code.*?{code}).*?', re.DOTALL)
@@ -94,7 +97,7 @@ pattern26 = re.compile('(?:https|http)://\w*\S*\d*', re.DOTALL)
 #pattern25 = re.compile(r'\(\d{2}:.*?\n', re.DOTALL)
 
 
-arr_patterns = [pattern0, pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9, pattern10, pattern11, pattern12, pattern13, pattern14, pattern15, pattern16, pattern17, pattern18, pattern19, pattern20, pattern21, pattern22, pattern23, pattern24, pattern25, pattern26]
+arr_patterns = [pattern0, pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9, pattern10, pattern11, pattern12, pattern13, pattern14, pattern15, pattern16, pattern17, pattern18, pattern19, pattern20, pattern21, pattern22, pattern23, pattern24, pattern25, pattern26, pattern27, pattern28, pattern29, pattern30]
 
 
 number_of_bug_descr = list()
@@ -115,9 +118,9 @@ for i in range(2,3):
     list_of_bugs_descriptions_and_stack_trace_flags = [list(x) for x in subset.values]
    
     for k,item in enumerate(list_of_bugs_descriptions_and_stack_trace_flags):
-        if k < 11:
+        if k < 92:
             continue
-        if k == 12:
+        if k == 93:
 #            print('|||||||||||||||||||||||||||||||||||||||||||')
 #            print('|||||||||||||||||||||||||||||||||||||||||||')
 #            print('|||||||||||||||||||||||||||||||||||||||||||')
